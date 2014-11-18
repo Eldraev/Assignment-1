aa237adbeb214e36d19c1f5d230b63f506bff12b
Florian Schiedt <florian.schiedt@googlemail.com>
2014-11-18 18:39:02 +0100

    Red

--- Assignment1/tests/tictactoe/BoardTests.java
+++ Assignment1/tests/tictactoe/BoardTests.java
@@ -2,6 +2,13 @@ package tictactoe;
 
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+
 public class BoardTests {
 
+	@Test
+	public void shouldCreateAInstance() {
+		new Board();
+	}
+	
 }
