253108191e0d1bfc25d7aa52e5144f217c87e3d1
Florian Schiedt <florian.schiedt@googlemail.com>
2014-11-19 14:19:59 +0100

    Green

--- Assignment1/src/tictactoe/Board.java
+++ Assignment1/src/tictactoe/Board.java
@@ -3,7 +3,7 @@ package tictactoe;
 public class Board {
 	
 	public Field getField(int x, int y) {
-		if(x<0 || y<0 || x>2)
+		if(x<0 || y<0 || x>2 || y>2)
 			throw new IllegalArgumentException();
 		return null;
 	}
