21c366f521d653613bc017a09953ba666c3af5de
Florian Schiedt <florian.schiedt@googlemail.com>
2014-11-19 13:27:58 +0100

    Red

--- Assignment1/tests/tictactoe/FieldTests.java
+++ Assignment1/tests/tictactoe/FieldTests.java
@@ -1,5 +1,15 @@
 package tictactoe;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class FieldTests {
 
+	@Test
+	public void shouldCreateAInstance() {
+		new Field();
+	}
+	
 }
+
