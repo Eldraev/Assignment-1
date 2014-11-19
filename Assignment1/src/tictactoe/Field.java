c3a9210d4fddceeaf85af18da9d14eeed67d8547
Florian Schiedt <florian.schiedt@googlemail.com>
2014-11-19 13:52:44 +0100

    Green

--- Assignment1/src/tictactoe/Field.java
+++ Assignment1/src/tictactoe/Field.java
@@ -2,16 +2,18 @@ package tictactoe;
 
 public class Field {
 	
+	public FieldState state;
+	
 	public Field() {
 		
 	}
 	
 	public Field(FieldState fs) {
-		
+		state = fs;
 	}
 	
 	public FieldState getValue() {
-		return null;
+		return state;
 	}
 	
 	public void setValue(FieldState fs) {
