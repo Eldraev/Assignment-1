0667e089430d18bd50d3b68c8d631a2fcdbce5e2
Florian Schiedt <florian.schiedt@googlemail.com>
2014-11-19 14:32:31 +0100

    Green

--- Assignment1/src/tictactoe/Board.java
+++ Assignment1/src/tictactoe/Board.java
@@ -1,11 +1,23 @@
 package tictactoe;
 
+import static org.junit.Assert.assertEquals;
+
 public class Board {
+	
+	private Field[][] fields;
+	
+	public Board() {
+		fields = new Field[3][3];
+		for(int x=0;x<3;x++) {
+			for(int y=0;y<3;y++) 
+				fields[x][y] = new Field();
+		}
+	}
 	
 	public Field getField(int x, int y) {
 		if(x<0 || y<0 || x>2 || y>2)
 			throw new IllegalArgumentException();
-		return null;
+		return fields[x][y];
 	}
 	
 }
