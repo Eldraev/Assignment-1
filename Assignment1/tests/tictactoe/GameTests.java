5ec5dcce33d81d2c1cd70a1e3293149d4410de0b
Florian Schiedt <florian.schiedt@googlemail.com>
2014-11-19 14:57:44 +0100

    Red

---
+++ Assignment1/tests/tictactoe/GameTests.java
@@ -0,0 +1,14 @@
+package tictactoe;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameTests {
+	
+	@Test
+	public void shouldCreateAInstance() {
+		new Game();
+	}
+	
+}
