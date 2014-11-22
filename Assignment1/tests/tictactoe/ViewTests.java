a706f1dc0f5981d15ee7112dee04f70d8763c47a
Florian Schiedt <florian.schiedt@googlemail.com>
2014-11-22 14:57:53 +0100

    Red

--- Assignment1/tests/tictactoe/ViewTests.java
+++ Assignment1/tests/tictactoe/ViewTests.java
@@ -2,15 +2,49 @@ package tictactoe;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import tictactoe.View.View;
 
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.anyString;
+
 public class ViewTests {
+	
+	private PrintStream originalOutput;
+	private PrintStream mockStream;
+	
+	@Before
+	public void mockOutput() {
+		originalOutput = System.out;
+		mockStream = mock(PrintStream.class);
+		System.setOut(mockStream);
+	}
 	
 	@Test
 	public void shouldCreateAInstance() {
 		new View();
+	}
+	
+	@Test
+	public void shouldWriteMessageToPlayer() {
+		View v = new View();
+		v.notifyPlayer();
+		verify(mockStream).println("It's your turn: ");
+	}
+	
+	@After
+	public void resetOutput() {
+		System.setOut(originalOutput);
 	}
 	
 }
