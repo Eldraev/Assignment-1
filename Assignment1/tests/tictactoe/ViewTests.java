b0d7b47ad7389fec48b5a77c646dd046c9581918
Florian Schiedt <florian.schiedt@googlemail.com>
2014-11-22 12:13:09 +0100

    Red

---
+++ Assignment1/tests/tictactoe/ViewTests.java
@@ -0,0 +1,14 @@
+package tictactoe;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ViewTests {
+	
+	@Test
+	public void shouldCreateAInstance() {
+		new View();
+	}
+	
+}
