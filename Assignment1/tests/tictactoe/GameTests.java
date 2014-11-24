7a2e5166074eeadb99c9845e2562847136def0f8
Florian Schiedt <florian.schiedt@googlemail.com>
2014-11-24 15:22:33 +0100

    Red

--- Assignment1/tests/tictactoe/GameTests.java
+++ Assignment1/tests/tictactoe/GameTests.java
@@ -1,10 +1,14 @@
 package tictactoe;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.Test;
+import org.mockito.InOrder;
 
 import tictactoe.Controller.Game;
+import tictactoe.Model.Board;
+import tictactoe.Model.Field;
 import tictactoe.View.View;
 
 public class GameTests {
@@ -18,6 +22,18 @@ public class GameTests {
 	public void shouldPlayGame() {
 		Game g = new Game(new View());
 		g.play();
+	}
+	
+	@Test
+	public void shouldShowDialog() {
+		View mockView = mock(View.class);
+		Game g = new Game(mockView);
+		g.play();
+		InOrder inOrder = inOrder(mockView);
+		for(int i=0;i<9;i++) {
+			inOrder.verify(mockView).drawBoard(any(Field[][].class));
+			inOrder.verify(mockView).notifyPlayer();
+		}
 	}
 	
 }
