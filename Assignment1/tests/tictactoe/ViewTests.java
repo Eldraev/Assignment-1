d2fa354beca7893e55a091110802467e1725e880
Florian Schiedt <florian.schiedt@googlemail.com>
2014-11-24 13:44:53 +0100

    Fixing the Test (still red)

--- Assignment1/tests/tictactoe/ViewTests.java
+++ Assignment1/tests/tictactoe/ViewTests.java
@@ -64,11 +64,11 @@ public class ViewTests {
 		}
 		v.drawBoard(testBoard);
 		InOrder inOrder = inOrder(mockStream);
-		inOrder.verify(mockStream, times(3)).print("X");
+		inOrder.verify(mockStream, times(3)).print(" ");
 		inOrder.verify(mockStream, times(1)).print("\n");
-		inOrder.verify(mockStream, times(3)).print("X");
+		inOrder.verify(mockStream, times(3)).print(" ");
 		inOrder.verify(mockStream, times(1)).print("\n");
-		inOrder.verify(mockStream, times(3)).print("X");
+		inOrder.verify(mockStream, times(3)).print(" ");
 		inOrder.verify(mockStream, times(1)).print("\n");
 		
 	}
