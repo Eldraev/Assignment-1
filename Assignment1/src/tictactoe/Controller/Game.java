8038679ed4646d1263514786151bf9c25c0ea1ff
Florian Schiedt <florian.schiedt@googlemail.com>
2014-11-24 15:22:39 +0100

    Green

--- Assignment1/src/tictactoe/Controller/Game.java
+++ Assignment1/src/tictactoe/Controller/Game.java
@@ -1,15 +1,21 @@
 package tictactoe.Controller;
 
+import tictactoe.Model.Board;
 import tictactoe.View.View;
 
 public class Game {
 
+	private View view;
+	
 	public Game(View v) {
-		
+		view = v;
 	}
 	
 	public void play() {
-		
+		for(int i = 0; i<9;i++) {
+			view.drawBoard(new Board().getBoardArray());
+			view.notifyPlayer();
+		}
 	}
 	
 }
