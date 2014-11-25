447a159b83801b9b86d3bd9c4eae81f5a155ea20
Florian Schiedt <florian.schiedt@googlemail.com>
2014-11-25 18:29:08 +0100

    Red

--- Assignment1/tests/tictactoe/ViewTests.java
+++ Assignment1/tests/tictactoe/ViewTests.java
@@ -97,11 +97,11 @@ public class ViewTests {
 		}
 		v.drawBoard(testBoard);
 		InOrder inOrder = inOrder(mockStream);
-		inOrder.verify(mockStream, times(3)).print("X");
+		inOrder.verify(mockStream, times(3)).print("X ");
 		inOrder.verify(mockStream, times(1)).print("\n");
-		inOrder.verify(mockStream, times(3)).print("X");
+		inOrder.verify(mockStream, times(3)).print("X ");
 		inOrder.verify(mockStream, times(1)).print("\n");
-		inOrder.verify(mockStream, times(3)).print("X");
+		inOrder.verify(mockStream, times(3)).print("X ");
 		inOrder.verify(mockStream, times(1)).print("\n");
 	}
 	
