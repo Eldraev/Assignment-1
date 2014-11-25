e3632ce0932c0b37847bc72b3ae60814d61bfc48
Florian Schiedt <florian.schiedt@googlemail.com>
2014-11-25 19:14:57 +0100

    Red

--- Assignment1/tests/tictactoe/GameTests.java
+++ Assignment1/tests/tictactoe/GameTests.java
@@ -118,7 +118,18 @@ public class GameTests {
 		verify(mockView).printWinner(true);
 	}
 	
-	
+	@Test
+	public void shouldLetAIWin() {
+		System.setIn(new ByteArrayInputStream("1 1".getBytes()));
+		View mockView = mock(View.class);
+		Game g = new Game(mockView);
+		g.setField(1, 2, FieldState.Cross);
+		g.setField(1, 3, FieldState.Cross);
+		g.setField(2, 2, FieldState.Cross);
+		g.setField(2, 3, FieldState.Cross);
+		g.play();
+		verify(mockView).printWinner(false);
+	}
 	
 	@Test
 	public void shouldReturnParsedNumber() {
