58f795817728e0f4404d5cbdd0857f12cd54e3a8
Florian Schiedt <florian.schiedt@googlemail.com>
2014-11-25 20:35:42 +0100

    Must have forgotten to commit this

--- Assignment1/src/tictactoe/TicTacToeMain.java
+++ Assignment1/src/tictactoe/TicTacToeMain.java
@@ -5,6 +5,10 @@ import tictactoe.View.View;
 
 public class TicTacToeMain {
 	
+	public static void main(String[] args) {
+		new TicTacToeMain().launch(new Game(new View()));
+	}
+	
 	public void launch(Game g) {
 		while(g.isPlaying())
 			g.play();
